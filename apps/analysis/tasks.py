5c6dede2aae0fc026f7753f03ff7c68a417c1f0e
Chris de Jong <github@dejong.lu>
2021-01-29 16:14:26 +0100

    Added tasks for balance sheet downloading

---
+++ apps/analysis/tasks.py
@@ -0,0 +1,17 @@
+import os
+from core.celery import app
+from ..stocks.models import Stock
+from iexclass import IexFinanceApi
+
+# Download the latest balance sheet information for the given stocks
+@app.task
+def download_balance_sheet(iex_tickers):
+    iex_finance_api = IexFinanceApi(iex_tickers)
+    
+
+# Celery task that is executed monthly in order to download the stock data
+@app.task
+def download_stock_analysis():
+    iexfinance_tickers = list(Stock.objects.all().values_list('iexfinance_ticker'))
+    download_balance_sheet(iexfinance_tickers)
+
