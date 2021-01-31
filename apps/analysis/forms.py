2ea8dfe0ec91eab251c6071516385d068782f637
Chris de Jong <github@dejong.lu>
2021-01-31 22:29:57 +0100

    Limit stock in analysis to user stocks

--- apps/analysis/forms.py
+++ apps/analysis/forms.py
@@ -4,6 +4,10 @@ import datetime
 
 # Form to retrieve date from user
 class StockForm(forms.Form):
-    stock = forms.ModelChoiceField(queryset=Stock.objects.all())
+    stock = forms.ModelChoiceField(queryset=Stock.objects.none())
+
+    def __init__(self, queryset):
+        super(StockForm, self).__init__()
+        self.fields['stock'].queryset = queryset
 
 
--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -1,8 +1,8 @@
 import datetime
 import os
 from .forms import StockForm
-from .models import BalanceSheet, KeyStats
-from ..stocks.models import Stock
+from .models import BalanceSheet, KeyStats, CashFlow, IncomeStatement
+from ..stocks.models import Transaction
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -10,14 +10,26 @@ from django.contrib.auth.models import User
 # Create your vigws here.
 @login_required(login_url="login")
 def stock_analysis_view(request, *args, **kwargs):
+    # Obtain user object to filter stock
+    stocks = Transaction.objects.filter(user=request.user).distinct()
+
     # Show stock form in the beginning
-    stock_form = StockForm()
+    stock_form = StockForm(stocks)
+    
     if request.method == "GET":
         balance_sheet = None
+        key_stats = None
+        income_statement = None
+        cash_flow = None
     elif request.method == 'POST':
+        
+
+        # Obtain alle the user stock which havve an entry for either key
         stock = get_object_or_404(Stock, id=stock_form.data['stock'])
         key_stats = KeyStats.objects.filter(stock=stock).order_by('-date')[0]
         balance_sheet = BalanceSheet.objects.filter(stock=stock).order_by('-date')[0]
+        income_statement = IncomeStatement.filter(stock=stock).order_by('-date')[0]
+        cash_flow = CashFlow.filter(stock=stock).order_by('-date')[0]
 
-    my_context = {"form": stock_form, 'balance_sheet': balance_sheet, 'key_stats': key_stats}
+    my_context = {"form": stock_form, 'balance_sheet': balance_sheet, 'key_stats': key_stats, 'income_statement': income_statement, 'cash_flow': cash_flow}
     return render(request, "stock_analysis.html", my_context)
